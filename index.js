88a2b87c2559d2aa49628b9d2808c38837220eb5
Cristain <mitashil99@gmail.com>
2022-10-25 21:35:17 +0400

    data changed

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const cors = require('cors')
 
 app.use(cors())
 
+const data = require('./Data/data.json')
+
 app.get('/',(req,res)=>{
     res.send('App On Fire');
 })
+app.get('/courses',(req,res)=>[
+    res.send(data)
+])
 
 app.listen(port,()=>{
     console.log('App Fire On Port 5000')
